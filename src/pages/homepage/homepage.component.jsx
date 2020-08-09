54d0680226e90f9d206d2254c7b581313cfb198f
Folafunmi <folafunmimustapha@gmail.com>
2020-08-09 23:52:05 +0100

    Added directory to homepage component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,12 +1,14 @@
 /*jshint esversion: 10 */
 
 import React from "react";
+import Directory from "../../components/directory/directory.component";
 import "./homepage.styles.scss";
 
 /** A functional component is used because no lifecycle
  * methods are needed this point, neither does the state
  * need to be stored
  */
+
 const HomePage = () => (
   <div className="homepage">
     <div className="directory-menu">
