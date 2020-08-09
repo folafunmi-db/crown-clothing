5ac1149ebe25a52ec7d7fede0cf082332a14b6e3
Folafunmi <folafunmimustapha@gmail.com>
2020-08-10 00:17:55 +0100

    Added imageUrl to menu component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,7 +12,7 @@ import MenuItem from "../menu-item/menu-item.component";
 class Directory extends React.Component {
 	constructor() {
 		super();
-		
+
 		this.state = {
 			sections: [
 				{
@@ -55,7 +55,7 @@ class Directory extends React.Component {
 		return (
 			<div className="directory-menu">
 				{this.state.sections.map(({ title, imageUrl, id }) => (
-					<MenuItem key={id} title={title} />
+					<MenuItem key={id} title={title} imageUrl={imageUrl} />
 				))}
 			</div>
 		);
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,7 +1,6 @@
 /*jshint esversion: 10 */
 
 import React from "react";
-
 import "./menu-item.styles.scss";
 
 /** The menu item doesn't need to hold any state, so we
@@ -9,13 +8,23 @@ import "./menu-item.styles.scss";
  */
 
 // Destructuring the title off of the props
-const MenuItem = ({ title }) => (
-  <div className="menu-item">
-    <div className="content">
-      <h1 className="title">{title}</h1>
-      <span className="subtitle">SHOP NOW</span>
-    </div>
-  </div>
+const MenuItem = ({ title, imageUrl }) => (
+	/** React provides every markup element with a css
+	 * property of 'style' and it takes an object that has
+   * prop values equal to css values that are to be applied
+   * 
+   * And css values would be in camelCase */
+	<div
+		style={{
+			backgroundImage: `url(${imageUrl})`,
+		}}
+		className="menu-item"
+	>
+		<div className="content">
+			<h1 className="title">{title}</h1>
+			<span className="subtitle">SHOP NOW</span>
+		</div>
+	</div>
 );
 
 export default MenuItem;
