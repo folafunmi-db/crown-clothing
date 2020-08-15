cadbdd066f4f0d22cc4a81e3a0d68ddfe4039ad3
Folafunmi <folafunmimustapha@gmail.com>
2020-08-15 11:26:01 +0100

    Added shop page

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,20 @@
+/*jshint esversion: 10 */
+
+import React from "react";
+import SHOP_DATA from "./shop.data.js";
+
+class ShopPage extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			collections: SHOP_DATA,
+		};
+	}
+
+	render() {
+		return <div>SHOP_DATA</div>;
+	}
+}
+
+export default ShopPage;
