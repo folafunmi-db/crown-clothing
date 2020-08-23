3d1adead2b684d826b845bf99f3dc183bbffc4a7
Folafunmi <folafunmimustapha@gmail.com>
2020-08-24 00:49:37 +0100

    Reformatted the code of sign in and sign up components

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
 	return (
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,10 +1,10 @@
 /*jshint esversion: 10 */
 
-import React from 'react';
-import './sign-in-and-sign-up.styles.scss';
+import React from "react";
+import "./sign-in-and-sign-up.styles.scss";
 
 const SignInAndSignUpPage = () => (
-  <div className='sign-in-and-sign-up'>SIGN IN</div>
+	<div className="sign-in-and-sign-up">SIGN IN</div>
 );
 
 export default SignInAndSignUpPage;
