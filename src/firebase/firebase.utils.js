1ae7c3fb97e4ee2059f244ccf8384a6c99310d0a
Folafunmi <folafunmimustapha@gmail.com>
2020-08-26 01:42:22 +0100

    Added firebase auth and sign in with Google

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,5 +1,30 @@
 /*jshint esversion: 10 */
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
 
+/** Only import the utilities that you need because
+ * they're quite heavy and would ony drag out app
+ */
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+	apiKey: "AIzaSyBZtd-ZMdS9cJuXHwQitRC3pYcDYgPRO3Q",
+	authDomain: "crwn-clothn-db.firebaseapp.com",
+	databaseURL: "https://crwn-clothn-db.firebaseio.com",
+	projectId: "crwn-clothn-db",
+	storageBucket: "crwn-clothn-db.appspot.com",
+	messagingSenderId: "73245815817",
+	appId: "1:73245815817:web:5a93a24ec0f8eeaa6b74d8",
+	measurementId: "G-EPERGLD6F1",
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestone = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
