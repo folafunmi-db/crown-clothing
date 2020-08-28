6d667cbdc2fb76a1ea52403f35316f26272328a6
Folafunmi <folafunmimustapha@gmail.com>
2020-08-28 20:57:10 +0100

    Added asynchronous function for firestore auth

--- src/App.js
+++ src/App.js
@@ -21,9 +21,21 @@ class App extends React.Component {
 	unsubscribeFromAuth = null;
 
 	componentDidMount() {
-		this.unsubscribeFromAtuh = auth.onAuthStateChanged(async user => {
-			// this.setState({currentUser: user});
-			createUserProfileDocument(user);
+		this.unsubscribeFromAtuh = auth.onAuthStateChanged(async (userAuth) => {
+			if (userAuth) {
+				const userRef = await createUserProfileDocument(userAuth);
+
+				userRef.onSnapshot((snapShot) => {
+					this.setState({
+						currentUser: {
+							id: snapShot.id,
+							...snapShot.data(),
+						},
+					});
+				});
+			} else {
+				this.setState({ currentUser: userAuth });
+			}
 		});
 	}
 
